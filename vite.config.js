4fd7de3fa199c8576429ee485b9d1a3b3b1758fb
Marco Antonio da Silva Ferreira <devloop.code@gmail.com>
2026-09-01 21:32:42 -0300

    Portfólio pronto e livre de conflitos

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-<<<<<<< HEAD
 import tailwindcss from '@tailwindcss/vite'
-export default defineConfig({ plugins: [react(), tailwindcss()] })
-=======
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
 })
->>>>>>> d340be09327d0baaf5e122d2b93c46dc96bf0e74
